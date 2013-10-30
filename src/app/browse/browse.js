1e978a3ff2dffda89783cc8b2720c4b293820680
Øystein Schrøder Elvik <oysteine@nbr-oysteine.(none)>
2013-10-30 13:52:14 +0100

    Renamed model to resource, dont mix with data model

--- src/app/browse/browse.js
+++ src/app/browse/browse.js
@@ -18,26 +18,26 @@ angular.module( 'crm.browse', [
     }
   });
 
-  $stateProvider.state( 'browse.model', {
-    url: '/{model:[a-z_-]{1,20}}',
+  $stateProvider.state( 'browse.resource', {
+    url: '/{resource:[a-z_-]{1,20}}',
     views: {
       "browser": {
         controller: 'BrowseCtrl',
-        templateUrl: 'browse/browse.model.tpl.html'
+        templateUrl: 'browse/browse.resource.tpl.html'
       }
     }
   });
 })
 
 .controller( 'BrowseCtrl', function BrowseController( $scope, $stateParams, $location, Page, Data, BrowserHistory ) {
-    $scope.model = Data.model[$stateParams.model];
+    $scope.model = Data.model[$stateParams.resource];
 
     BrowserHistory.clear();
     BrowserHistory.add($scope.model.title, '#' + $location.path());
 
     $scope.columns = [{ value: $scope.model.label, label: 'Name', primary: true }];
 
-    $scope.records = Data.get($stateParams.model);
+    $scope.records = Data.get($stateParams.resource);
 
     Page.setTitle($scope.model.title);
 })
